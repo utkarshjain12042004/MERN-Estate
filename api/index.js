5e2fc185e7bc2a213543fc25d844c571cea883f7
Utkarsh <utkarshjain12042004@gmail.com>
2023-12-05 14:11:38 +0000

    Created sign up api routes

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose
@@ -9,16 +10,17 @@ mongoose
   .then(() => {
     console.log('Connected to MongoDB!');
   })
-  .catch((err) => { 
-    console.log('Failed to connect to MongoDB', err);
+  .catch((err) => {
+    console.log(err);
   });
-
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
-}
-);
+});
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
