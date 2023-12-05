3a569a8d4f30e01ea8b4ecca30f5068d8f98614b
Utkarsh <utkarshjain12042004@gmail.com>
2023-12-05 13:56:58 +0000

    Add test user route to API

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  purge: {
-    content: [
+  content: [
       "./index.html",
       "./src/**/*.{js,ts,jsx,tsx}",
     ],
-  },
   theme: {
     extend: {},
   },
