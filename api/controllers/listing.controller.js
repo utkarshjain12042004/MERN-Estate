cbc0994f3a4f28a3536ddf2193264bf18e32024d
Utkarsh <utkarshjain12042004@gmail.com>
2024-01-03 23:13:08 +0000

    Completed the delete user listing button functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -8,3 +8,22 @@ export const createListing = async (req, res, next) => {
     next(error)
   }
 };
+
+export const deleteListing = async (req, res, next) => { 
+  const listing = await Listing.findById(req.params.id);
+
+  if (!listing) {
+    return res.status(404).json({ error: "Listing not found" });
+  }
+
+  if (req.user.id !== listing.userRef) {
+    return res.status(401).json({ error: "You can only delete your own listings" });
+  }
+
+  try {
+    await Listing.findByIdAndDelete(req.params.id);
+    return res.status(200).json({ message: "Listing deleted successfully" });
+  } catch (error) {
+    next(error)
+  }
+};
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -150,6 +150,25 @@ export default function Profile() {
       setShowListingError(true);
     }
   };
+
+  const handleDeleteListing = async (listingId) => { 
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: 'DELETE'
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+        console.log(error.message);
+    }
+  };
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -245,7 +264,7 @@ export default function Profile() {
 
       {userListings && userListings.length > 0 && 
         <div className='flex flex-col gap-4'>
-          <h1 className='text-2xl font-semibold text-center mt-7'>Your Listings</h1>
+          <h1 className='text-2xl font-bold text-center mt-7'>Your Listings</h1>
           {userListings.map((listing) =>
             <div
               key={listing._id}
@@ -264,7 +283,7 @@ export default function Profile() {
                 <p >{listing.name}</p>
               </Link>
               <div className='flex flex-col items-center'>
-                <button className='text-red-700 uppercase'>Delete</button>
+                <button onClick={()=>handleDeleteListing(listing._id)} className='text-red-700 uppercase'>Delete</button>
                 <button className='text-green-700 uppercase'>Edit</button>
               </div>
             </div>
