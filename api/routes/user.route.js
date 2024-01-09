2409889814fe03c42d63ff2af3bea3070d3f267e
Utkarsh <utkarshjain12042004@gmail.com>
2024-01-09 16:43:54 +0000

    Completed the view listing page which includes the contact landloard feature

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,7 +3,8 @@ import {
   deleteUser,
   test,
   updateUserInfo,
-  getUserListings
+  getUserListings,
+  getUser,
 } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
@@ -14,5 +15,6 @@ router.get('/test', test);
 router.post('/update/:id', verifyToken, updateUserInfo)
 router.delete('/delete/:id', verifyToken, deleteUser)
 router.get('/listings/:id', verifyToken, getUserListings)
+router.get('/:id', verifyToken, getUser)
 
 export default router;
--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -284,7 +284,9 @@ export default function CreateListing() {
               />
               <div className='flex flex-col items-center'>
                 <p>Regular Price</p>
-                <span className='text-xs'>(£ / month)</span>
+                {formData.type === 'rent' && (
+                  <span className='text-xs'>($ / month)</span>
+                )}
               </div>
             </div>
             {formData.offer && (
@@ -300,7 +302,9 @@ export default function CreateListing() {
                 />
                 <div className='flex flex-col items-center'>
                   <p>Discounted Price</p>
-                  <span className='text-xs'>(£ / month)</span>
+                  {formData.type === 'rent' && (
+                    <span className='text-xs'>($ / month)</span>
+                  )}
                 </div>
               </div>
             )}
