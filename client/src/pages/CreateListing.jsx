6d03bd0ecb9c0a660fb7df5d6124ff112b57f2fe
Utkarsh <utkarshjain12042004@gmail.com>
2024-01-11 23:28:03 +0000

    Created the search functionality and connected the results to the listing pages.

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -285,7 +285,7 @@ export default function CreateListing() {
               <div className='flex flex-col items-center'>
                 <p>Regular Price</p>
                 {formData.type === 'rent' && (
-                  <span className='text-xs'>($ / month)</span>
+                  <span className='text-xs'>(£ / month)</span>
                 )}
               </div>
             </div>
@@ -303,7 +303,7 @@ export default function CreateListing() {
                 <div className='flex flex-col items-center'>
                   <p>Discounted Price</p>
                   {formData.type === 'rent' && (
-                    <span className='text-xs'>($ / month)</span>
+                    <span className='text-xs'>(£ / month)</span>
                   )}
                 </div>
               </div>
